5e063581b7fd81b9c2d05e5e94856c12df48738a
threatcon <otek@yahoo.com>
2023-12-17 19:24:20 -0600

    adjust linefeeds

--- src/extension.ts
+++ src/extension.ts
@@ -31,10 +31,31 @@ export function activate(context: vscode.ExtensionContext) {
 					vscode.window.showErrorMessage("ERROR! File Exists!");
 					return;
 				}
+                
+                // const uri = vscode.Uri.file(fullFileName);
+                // const wsedit = new vscode.WorkspaceEdit();
+                // wsedit.createFile(uri,{contents: new TextEncoder().encode(fileContent)});
+                // let editDone = await vscode.workspace.applyEdit(wsedit);
+                // console.log(editDone);
+                // // wsedit.insert(uri, new vscode.Position(0, 0), fileContent);
+                // // let successs = await vscode.workspace.applyEdit(wsedit);
+                // // console.log(successs);
+                // //await vscode.workspace.fs.writeFile(uri, new TextEncoder().encode(fileContent));
+
 				fs.writeFileSync(fullFileName, fileContent);
+              
+                let  bottomPosition;
+                
+                if (fileExtension === '.cls') {
+                bottomPosition = 14; 
+                } else {
+                bottomPosition = 3;
+                }            
+
+                let pos = new vscode.Position(bottomPosition,0);
 
 				vscode.workspace.openTextDocument(vscode.Uri.file(fullFileName)).then(file => {
-					vscode.window.showTextDocument(file);
+					vscode.window.showTextDocument(file, {selection: new vscode.Range(pos,pos)});
 				});
 			}
             }
@@ -47,25 +68,22 @@ export function activate(context: vscode.ExtensionContext) {
 
 function getFileContent(fileExtension: string, fileName: string): string {
     if (fileExtension === '.cls') {
-        return `
-VERSION 1.0 CLASS
-BEGIN
-MultiUse = -1  'True
-END
-Attribute VB_Name = "` + fileName + `"
-Attribute VB_GlobalNameSpace = False
-Attribute VB_Creatable = False
-Attribute VB_PredeclaredId = False
-Attribute VB_Exposed = False
-
-Option Explicit
-        `;
+        return "VERSION 1.0 CLASS\r\n" +
+"BEGIN\r\n" +
+"MultiUse = -1  'True\r\n" +
+"END\r\n" +
+"Attribute VB_Name = \"" + fileName + "\"\r\n" +
+"Attribute VB_GlobalNameSpace = False\r\n" +
+"Attribute VB_Creatable = False\r\n" +
+"Attribute VB_PredeclaredId = False\r\n" +
+"Attribute VB_Exposed = False\r\n" +
+"\r\n"+ 
+"Option Explicit\r\n"
+;
     } else if (fileExtension === '.bas') {
-        return `
-Attribute VB_Name = "` + fileName + `"
-
-Option Explicit
-`;
+        return "Attribute VB_Name = \"" + fileName + "\"\r\n" +
+"\r\n" +
+"Option Explicit\r\n" ;
     }
 
     return '';
